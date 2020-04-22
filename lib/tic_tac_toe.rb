f6b35142ca48a0df0006992a345fb168e968e0f1
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-22 12:44:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -109,7 +109,11 @@ def winner(board)
 end
 
 def play(board)
+<<<<<<< HEAD
   until over?(board) || draw?(board)
+=======
+  until over?(board)
+>>>>>>> e7e7085bb9b10d9c498328b2a21c8dfe967808da
     turn(board)
   end
   if won?(board)
